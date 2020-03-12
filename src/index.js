0d47d612565c61ba0556c05c029d5daf20f771e1
Alex Firmani <jafirmani@gmail.com>
2020-03-12 12:16:51 -0700

    Switching to icon-pocket-provider

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@
  * https://docs.pokt.network/docs/what-is-pocket-network
  * https://pokt.network
  */
-const PocketJS = require('@pokt-network/pocket-js')
+const PocketProvider = require('icon-pocket-provider')
 const IconService = require('icon-sdk-js')
 
 const { IconWallet, IconBuilder, IconAmount, IconConverter, SignedTransaction } = IconService
@@ -17,6 +17,7 @@ const { IconWallet, IconBuilder, IconAmount, IconConverter, SignedTransaction } 
  * https://www.icondev.io/docs/testnet
  */
 const iconChainID = "d9d77bce50d80e70026bd240fb0759f08aab7aee63d0a6d98c545f2b5ae0a0b8"
+const iconAPIPath = "/api/v3"
 const iconNetworkID = 80
 
 // Addresses on the Zicon testnet used for example transactions
@@ -32,49 +33,28 @@ const pocketPassphrase = 'yo'
 
 // Setup the Pocket Network
 const pocketNode = new URL('http://0.0.0.0:8081')
-const { Configuration, PocketAAT, HttpRpcProvider, RpcError } = PocketJS
-const pocketInstance = new PocketJS.Pocket([pocketNode], new HttpRpcProvider(pocketNode), new Configuration(5, 100000, 10000000))
+const { Configuration, PocketAAT, HttpRpcProvider } = PocketProvider
+const pocketInstance = new PocketProvider.Pocket([pocketNode], new HttpRpcProvider(pocketNode), new Configuration(5, 100000, 10000000))
 
+/**
+ * Boilerplate class based on ICON-SDK-JS quickstart examples.
+ * 
+ * The one change needed to use Pocket Network is to instantiate a Pocket Provider:
+ * this.provider = new PocketProvider.PocketProvider(iconChainID, iconAPIPath, pocketInstance, pocketAAT)
+ * 
+ * instead of a new IconProvider().
+ */
 class IcxTransactionExample {
     /**
      * Creates a new IconService from the Icon SDK using the Pocket Provider.
      * 
      * @param {pocketAAT} pocketAAT - Application Authentication Token
      */
-	constructor(pocketAAT) {
-        this.provider = new IconService.PocketProvider("/api/v3", this.relay, pocketAAT)
+	constructor(iconChainID, iconAPIPath, pocketInstance, pocketAAT) {
+        this.provider = new PocketProvider.PocketProvider(iconChainID, iconAPIPath, pocketInstance, pocketAAT)
         this.iconService = new IconService(this.provider)
         this.wallet = new IconWallet(Buffer.from(iconPrivateKey1, 'hex'))
         this.txHash = ''
-    }
-    
-    /**
-     * The relay function is passed to the Icon SDK as a callback for servicing the
-     * SDK's HTTP requests.
-     * 
-     * @param {string} url - Icon API URL
-     * @param {string} body - Request body to relay
-     * @param {pocketAAT} pocketAAT - AAT to use for bandwidth
-     */
-    async relay(url, body, pocketAAT) {
-        try {
-            const relayResponse = await pocketInstance.sendRelay(
-                body, 
-                iconChainID, 
-                pocketAAT, 
-                undefined, 
-                undefined, 
-                "POST", 
-                url
-            )
-            if (relayResponse instanceof RpcError) {
-                console.log("Relay error:", relayResponse.message)
-                throw new Error(relayResponse.message)
-            }
-            return JSON.parse(relayResponse.response)
-        } catch(e) {
-            return e
-        }
     }   
     
     /**
@@ -122,7 +102,7 @@ class IcxTransactionExample {
 
 // Create an AAT and use it for the example Icon transactions
 const pocketAAT = PocketAAT.from('0.0.1', pocketPublicKey, pocketPublicKey, pocketPrivateKey)
-const demo = new IcxTransactionExample(pocketAAT);
+const demo = new IcxTransactionExample(iconChainID, iconAPIPath, pocketInstance, pocketAAT);
 
 pocketInstance.keybase.importAccount(Buffer.from(pocketPrivateKey, 'hex'), pocketPassphrase).then(() => {
     pocketInstance.keybase.unlockAccount(pocketAddress, pocketPassphrase, 0).then(() => {
@@ -144,3 +124,5 @@ pocketInstance.keybase.importAccount(Buffer.from(pocketPrivateKey, 'hex'), pocke
         })()
     })
 })
+
+console.log("Running...")
